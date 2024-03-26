55b4c790f53eba76566b4ce53c02745cbd29bdd3
facerman <tayjinheng@gmail.com>
2024-03-26 23:20:35 +0800

    uncompleted GameView, Game, missing GameThread

---
+++ app/src/main/java/com/example/game/GameActivity.java
@@ -0,0 +1,26 @@
+package com.example.game;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        hideStatusBar();
+        setContentView(new GameView(this));
+    }
+
+    private void hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController wic = getWindow().getInsetsController();
+            if (wic != null) {
+                wic.hide(WindowInsets.Type.statusBars());
+            }
+        }
+    }
+}
